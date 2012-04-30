e06f093615517f7270c482b16f6458731a5b33ac
Eric Stratmann <estrat@cs.stanford.edu>
2012-04-30 14:45:39 -0700

    First attempt at a Policy file. Messages can be read by the sender or receiver

---
+++ messanger-policy/Messanger/Policy.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DeriveDataTypeable, OverloadedStrings #-}
+module Messanger.Policy where
+
+import Data.Maybe
+import Hails.Policy
+import Messanger.Models
+
+-- | Internal messanger policy. The type constructor should not be
+-- exported to avoid leaking the privilege.
+data MessangerPolicy = MessangerPolicy TCBPriv (Database DCLabel)
+  deriving (Typeable)
+
+instance DatabasePolicy MessangerPolicy where
+  createDatabasePolicy conf p = do
+    messageColl <- messagesCollection p
+    db <- labelDatabase conf collectionsLabel lpub
+          >>= assocCollectionP p messageColl
+    return $ MessangerPolicy p db
+    where collectionsLabel = newDC (<>) (priv p)
+
+  policyDB (MessangerPolicy _ db) = db
+
+messagesCollection :: TCBPriv -> DC (Collection DCLabel)
+messagesCollection p = collectionP p "messages" lpub colClearance $
+  RawPolicy (labelForMessage . fromJust . fromDocument)
+            [ ("_id",   SearchableField)
+            , ("from", SearchableField)
+            , ("to", SearchableField)
+            ]
+    where colClearance = newDC (priv p) (<>)
+          labelForMessage message = 
+            let r = messageTo message .\/. messageFrom message .\/. priv p
+            in newDC (r) ((messageTo message) .\/. priv p)
+
+-- | Policy handler
+messangerDB :: DC MessangerPolicy
+messangerDB = mkPolicy
